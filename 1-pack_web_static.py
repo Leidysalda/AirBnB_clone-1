7b2310c7117757eff4796c92bb8efd6eb2983e25
leidysalda <leidysalda1@gmail.com>
2020-08-18 17:32:14 +0000

    [Fix] style pep8 file 1-

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,7 +3,7 @@
 Fabric script that generates a .tgz archive from the contents of the web_static
 """
 from datetime import datetime
-from fabric.api import local 
+from fabric.api import local
 
 
 def do_pack():
@@ -11,6 +11,5 @@ def do_pack():
     now = datetime.now()
     time = now.strftime("%Y%m%d%H%M%S")
     ruta = "web_static_{}.tgz".format(time)
-    
     local('mkdir -p versions')
     local('tar -czvf versions/{:s} web_static/'.format(ruta))
