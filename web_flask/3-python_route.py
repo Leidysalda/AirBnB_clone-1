87ab672c5883d50efec4467f6bad7e9bdc58f4f0
leidysalda <leidysalda1@gmail.com>
2020-09-01 21:48:45 +0000

    [Fix] #!

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,17 +1,17 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 """
 script that starts a Flask web application
 """
-
-
 from flask import Flask
+
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """hello"""
-    return 'Hello HBNB'
+    return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
